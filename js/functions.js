0fb44c33579f7831dd24c3a1604c315a3dc7256d
lacey washington <lacey.m.washington@gmail.com>
2023-02-10 00:42:46 -0600

    added used strict

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+'use strict';
 let userName = prompt('hello what is your name?').toUpperCase();
 let gender = prompt('what gender do you identify as?' + 'please answer with: boy, girl, or non-binary').toLowerCase();
 alert('Welcome ' + userName + ' thanks for visiting my bio I made a couple of questions for you to answer to get to know me better before, you actually read my about my life. If you would be so kind as to answer with yes or no or y or n. Thanks in advance. ');
@@ -66,7 +67,7 @@ if (answer4 === 'yes' || answer4 === 'y') {
     answer4;
 }
 
-alert('Thanks for playing the game I worked pretty hard on it. Dont\'t worry if you didn\'t get a lot or all of the answers right. I mean we just met but, you are welcomed to try again after you read my bio. Thanks again'  +  userName  )
+alert('Thanks for playing the game I worked pretty hard on it. Dont\'t worry if you didn\'t get a lot or all of the answers right. I mean we just met but, you are welcomed to try again after you read my bio. Thanks again'  +  userName );
 
 
 if (gender === 'boy') {
